a2ef5719a7f62722e3c35a6dc8fe0b5963e6d134
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-09-12 16:07:40 +0400

    Dz

--- DZ_47(1)/Program.cs
+++ DZ_47(1)/Program.cs
@@ -1,12 +1,12 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 // m = 3, n = 4.
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+double[,] CreateMatrixRndInt(int row, int col, int min, int max)
 {
-    int[,] matrix = new int[row, col];
+    double[,] matrix = new double[row, col];
     Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
@@ -32,5 +32,5 @@ int[,] CreateMatrixRndInt(int row, int col, int min, int max)
         }
    
     }
- int [,] array2D = CreateMatrixRndInt(3, 4, -9, 9);
+ int [,] array2D = CreateMatrixRndInt(100);
  PrintMatrix(array2D);
