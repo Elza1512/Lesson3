d87f12cf369213839623b6693f9d884d943ec421
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-09-12 13:35:45 +0400

    DZ-43

--- DZ_43/Program.cs
+++ DZ_43/Program.cs
@@ -2,18 +2,18 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите сторону b1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите сторону k1: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите сторону b2: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите сторону k2: ");
-int num4 = Convert.ToInt32(Console.ReadLine());
-
-bool Equation(int x, int y)
-{
-    return y = k1 * x + b1 == y = k2 * x + b2;
-}
-bool result = Equation (num1, num2, num3, num4);
-Console.WriteLine(result?"Является треугольником":"Не является треугольником");
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Точка пересечения: ({x};{y})");
