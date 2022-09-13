4f1d658f7517f1e02de287d583c7cdf227bba1c3
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-09-13 12:33:00 +0400

    DZ-52

--- DZ_52/Program.cs
+++ DZ_52/Program.cs
@@ -4,3 +4,29 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[,] a=new double[5,5];
+double[] b = new double[5];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        a[i, j] = i * j + 1;
+        Console.Write(a[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 1, k = 0; i < 5; i += 2)
+{
+    double summa = 0;
+    for (int j = 0; j < 5; j++)
+    {
+        summa += a[i, j];
+    }
+    b[k] = summa / 5;
+    Console.WriteLine("Среднее арифметическое столбца " + (i) + " равно: " + b[k]);
+    k++;
+}
+Console.ReadKey(true);
